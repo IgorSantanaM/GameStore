a403cf63ab98763dee9f2161f926de7aa29e72d7
unknown <igorsantanamedeiros17@gmail.com>
2024-05-20 21:58:00 -0300

    Initial Commit

---
+++ GameStore.Api/Entities/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace GameStore.Api.Entities;
+
+public class Game
+{
+     public int Id {get; set; }
+
+   public required String Name {get; set; }
+
+   public int genreId {get; set;}
+
+   public Genre? Genre {get; set; }
+
+   public decimal price {get; set; }
+
+   public DateOnly ReleaseDate {get; set; }
+
+
+}
