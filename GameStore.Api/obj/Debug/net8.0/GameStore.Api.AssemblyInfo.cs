bed86f1c3fc60bc81616c6c3cc4cfae71597123b
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-07-14 20:44:24 -0300

    Addded gitignore file

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673571bfa8dd311791d05e7e857d00069242d4d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
